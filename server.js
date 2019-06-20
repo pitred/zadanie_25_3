751215780a457ab224e8310149d85a9b1ede3d80
Piotr Nowinski <pitred007git@gmail.com>
2019-06-21 00:02:08 +0200

    Finish project

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var stringifyFile = '';
 
-app.get('/:id', function(req, res) {
-   res.send('Id that was append to it ' + req.params.id);
+app.use(bodyParser.json());
+
+app.get('/getNote', function(req, res) {
+   fs.readFile('./test.json', 'utf-8', function(err, data) {
+      if (err) throw err;
+      stringifyFile = data;
+      res.send(data);
+   });
+});
+
+app.post('/updateNote/:note', function(req, res) {
+   stringifyFile = req.params.note;
+   fs.writeFile('./test.json', stringifyFile, function(err) {
+      if (err) throw err;
+      console.log('file updated');
+   });
 });
 
 app.listen(3000);
-
-app.use(function(req, res, next) {
-   res.status(404).send('Sorry i cant find anything you want !');
-});
